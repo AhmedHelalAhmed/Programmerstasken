4af3b7247cac4609c6616d7bfa3fc093f7cfdcde
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2022-01-03 01:45:50 +0200

    Fix input file name

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use Services\PrintingAverageWaitingTimeService;
 require __DIR__ . '/vendor/autoload.php';
 
 try {
-    (new PrintingAverageWaitingTimeService)->execute('inputt.txt');
+    (new PrintingAverageWaitingTimeService)->execute('input.txt');
 
 } catch (\Exception $e) {
     // report to loggers
